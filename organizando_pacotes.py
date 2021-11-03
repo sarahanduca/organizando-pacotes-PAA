5f0016aca4c492dfbeec17113725b653916d291e
Sarah <anduca.sarah@gmail.com>
2021-11-03 17:23:47 -0300

    add memo

--- organizando_pacotes.py
+++ organizando_pacotes.py
@@ -1,28 +1,28 @@
-def organizando_pacotes(entry, i, n, k):
-    min = 1000
-    weight = 0
-    if(k == 1):
-        for el in entry[i:]:
-            # entry -> [[x, w]]
-            # el -> [x, w]
-            if(el[0] < entry[n-1][0]):
-                sub = entry[n-1][0] - el[0]
-                weight = weight + el[1]*sub
-            print(weight)
-        return weight
+global memo
+memo = [None for i in range(20)]
+def find_cost(nodes, k, min_cost):
+    if(memo[k] == None):
+        if k == 1:
+            cost = 0
+            for node in nodes:
+                cost = cost + node[1] * (nodes[-1][0] - node[0])
+            return cost
+        else:
+            for z in range(len(nodes) - k):
+                current_min_cost = find_cost(nodes[:z+1], 1, min_cost) + find_cost(nodes[z+1:],k - 1, min_cost)
+                memo[k] = current_min_cost
+                if min_cost == None:
+                    min_cost = current_min_cost
+                elif current_min_cost < min_cost:
+                    min_cost = current_min_cost
+    return memo[k]
 
-    for z in range(1, n-1):
-        result = organizando_pacotes(entry, i, z, 1) + organizando_pacotes(entry, z + 1, n, 1)
-        # print(result)
+res1 = find_cost([(20, 1), (30, 1), (40, 1)], 1, None)
+res2 = find_cost([(11, 3), (12, 2), (13, 1)], 1, None)
+res3 = find_cost([(10, 15), (12, 17), (16, 18), (18, 13), (30, 10), (32, 1)], 2, None)
+res4 = find_cost([(10, 15), (12, 17), (16, 18), (18, 13), (30, 10), (32, 1)], 3, None)
 
-        if(result < min):
-            min = result
-            # print(min)
-    return min
-
-# assert(organizando_pacotes([[20, 1], [30, 1], [40, 1]], 1, 3, 1), 30)
-
-print(organizando_pacotes([[20, 1], [30, 1], [40, 1]], 0, 3, 1)) 
-# print(organizando_pacotes([[11, 3], [12, 2], [13, 1]], 1, 3, 1)) 
-# print(organizando_pacotes([[10, 15], [12, 17], [16, 18], [18, 13], [30, 10], [32, 1]], 1, 6, 2)) 
-# print(organizando_pacotes([[10, 15], [12, 17], [16, 18], [18, 13], [30, 10], [32, 1]], 1, 6, 3)) 
+print(res1) #prints 30
+print(res2) #prints 8
+print(res3) #prints 278
+print(res4) #prints 86
